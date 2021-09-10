229d58a665ae70979b484e30b6ed593e7888b472
Michael Pascuzzi <gatorpazz@gmail.com>
2021-09-09 20:56:17 -0400

    class lecture code

---
+++ javascriptIntro/main.js
@@ -0,0 +1,62 @@
+// Types
+
+// Strings
+console.log("Hello World")
+console.log('Hello World')
+
+// Numbers
+console.log(5);
+console.log(5.99);
+
+// Boolean
+console.log(true)
+console.log(false)
+
+// Undefined / Null
+console.log(undefined)
+console.log(null)
+
+// Variables
+// Naming is important, bad names below
+var x = "hello"
+let y = 55
+const z = true
+console.log(x);
+// Example of a good name
+let firstName = "Michael";
+
+// Reasignment of variables
+x = "goodbye";
+console.log(x);
+y = 234234;
+
+console.log(firstName);
+// Error, the variable last name hasn't been declared
+// console.log(lastName);
+
+// String Operators
+let lastName = " Pascuzzi"
+// Concatenation
+console.log(firstName + lastName)
+console.log("Duran" + " Duran")
+
+// Number Operators
+// addition +
+console.log(42 + 48);
+let sum = 42 + 48;
+console.log(sum);
+// substraction -
+console.log(55 - 5);
+let num1 = 5;
+let num2 = 5;
+// multiply *
+console.log(num1 * num2);
+// divide /
+console.log(num1 / num2)
+// modulo %
+console.log(num1 % 2);
+console.log(8 % 2);
+
+alert("Hello World")
+let age = parseInt(prompt("What is your age?"));
+console.log(age);
