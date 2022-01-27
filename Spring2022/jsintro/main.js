ff6046cf1553886fb48bf6982194c4e88ddaffdf
Michael Pascuzzi <gatorpazz@gmail.com>
2022-01-27 18:01:34 -0500

    Lecture 01/27

---
+++ Spring2022/jsintro/main.js
@@ -0,0 +1,61 @@
+// DATA TYPES
+  // // Strings
+  // 'hello' "world"
+  // // Numbers
+  // 123123124 32342.324234
+  // // Boolean
+  // true false
+  // // Undefined
+  // undefined
+  // // Null
+  // null
+
+// alert(3425)
+
+// Variables
+
+// var name = 'Michael Pascuzzi';
+// name = 'John Doe';
+// alert(name);
+// let age = 29;
+// age = 44;
+// console.log(age)
+// const height = 53;
+// height = 99
+
+// Data Manipulation
+// Math Operators;
+let x = 5;
+let y = 10;
+let sum = x + y + (15 - 5) * 23.42 / 5;
+console.log(sum);
+let diff = x - y;
+console.log(diff);
+let product = x * y;
+console.log(product);
+let quotient = x / y;
+console.log(quotient);
+// Modulo Operator
+let remainder = 15 % 2;
+console.log(remainder)
+// String Operator
+let firstName = 'Michael ';
+let lastName = 'Pascuzzi';
+// Concatenation
+console.log(firstName + lastName);
+// Concatenation with Type Coercion
+console.log(firstName + x)
+console.log('15' + '72')
+
+// Uninitialized Variable
+let weight;
+console.log(weight);
+weight = null;
+console.log(weight);
+
+// Boolean Values
+let isHungry = true;
+let notABoolean = 'true'
+let isTired = false;
+
+console.log ("My name is Michael Pascuzzi")
