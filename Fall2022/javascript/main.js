fdc503494fd990bf73bff1c740c860de68ca2c5e
Michael Pascuzzi <gatorpazz@gmail.com>
2022-09-15 17:27:18 -0500

    09/15 JavaScript intro lecture

---
+++ Fall2022/javascript/main.js
@@ -0,0 +1,78 @@
+// alert("hello world")
+// console.log("Hello World")
+
+// Simple Types
+
+// Strings
+// Single quotes or double quotes, both are almost identical
+// "Michael ' Pascuzzi"
+// '12353 Michigan" Ave'
+// '""You miss ever shot you don\'t take" - Michael Jordan" - Mich'
+// '32342'
+// "true"
+// Template Literals (A little more advanced, will talk about later)
+// `1 + 1 = ${1 + 1}`
+
+// String operations
+// get the length of a string
+// console.log("Michael Pascuzzi".length);
+// concatenation
+// console.log("Michael" + "Pascuzzi");
+
+// Numbers
+// int
+// 34823
+// float
+// 342.3423423
+
+// Number operations
+// console.log(5 + 5) // 10
+// console.log(54 - 5 - 10 - 33) // 6
+// console.log(5 * 5)
+// console.log(5 / 5)
+
+// Boolean
+// true
+// false
+
+// Undefined / null
+
+// type coercion
+// console.log("Michael" + 5);
+// console.log(58 / 2);
+
+// Variables
+// let variables
+let firstName = "Michael";
+let lastName = "Pascuzzi";
+console.log(firstName);
+
+// variable declaration
+let age;
+console.log(age);
+
+// variable assignment
+age = 30;
+console.log(age);
+
+firstName = "Jeff";
+console.log(firstName);
+
+let occupation = null;
+console.log(occupation);
+
+// booleans
+let lightOn = false;
+let canDrink = true;
+
+// Variable Operations (same as simple type operations)
+let test1 = 92
+let test2 = 84
+let test3 = 67
+
+console.log("Your average test score is " + (test1 + test2 + test3) / 3)
+
+// constant variables
+const name = "Michael Pascuzzi";
+
+name = "John Doe";
