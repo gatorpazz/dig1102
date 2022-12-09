cf9ed7147c059303df9d6ee0526883b7588ffca1
Michael Pascuzzi <gatorpazz@gmail.com>
2022-12-08 22:25:58 -0500

    code

--- Fall2022/dynamic_rendering/main.js
+++ Fall2022/dynamic_rendering/main.js
@@ -18,10 +18,11 @@ const listEl = document.querySelector('ul');
 const renderStudents = () => {
   const liEls = students.map((student, index) => (
     `
-      <li>
+      <li class="${student.enrolled ? '' : "unenrolled"}">
         <p>Name: ${student.name}</p>
         <p>Enrolled: ${student.enrolled}</p>
-        <button data-index="${index}">Delete Student</button>
+        <button class="delete" data-index="${index}">Delete Student</button>
+        <button class="unenroll" data-index="${index}">${student.enrolled ? '✅' : '&#127367;'}</button>
       </li>
     `
   )).join('');
@@ -30,9 +31,21 @@ const renderStudents = () => {
 renderStudents();
 
 listEl.addEventListener('click', function(event) {
-  if (event.target.matches('button')) {
+  if (event.target.matches('.delete')) {
     const index = event.target.dataset.index;
     students.splice(index, 1);
     renderStudents();
   }
+  if (event.target.matches('.unenroll')) {
+    const index = event.target.dataset.index;
+    students[index].enrolled = !students[index].enrolled;
+    renderStudents();
+  }
 })
+
+const menuButton = document.querySelector('#menuButton');
+const menuEl = document.querySelector('ol');
+
+menuButton.addEventListener('click', function() {
+  menuEl.classList.toggle('hidden');
+});
